208b12e98a71e136b65f1ecf206bb87dbb9b60fa
學 <59658153+js051@users.noreply.github.com>
2026-08-10 01:44:04 +0800

    chore(repo): 發布 0.1.9 開發預覽版 (#125)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "dev.openeos.control"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.1.8"
+        versionCode = 10
+        versionName = "0.1.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
